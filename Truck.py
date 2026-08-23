f8cb3c5a470316d07271440de1a3dcf3f052fa51
VirtualVince <vincentm_2@hotmail.com>
2026-08-23 03:41:52 +0000

    package 9 address change functionality needs implementation. all other deliveries successfully delivered under 140 miles useing nearest neighbor algorithm. truck loading functionality is currently manual and will need a rework but will currently still pass

--- Truck.py
+++ Truck.py
@@ -1,5 +1,7 @@
 # Truck.py
 
+from DeliveryStatus import DeliveryStatus
+
 class Truck:
     def __init__(self, truck_number, time, location, distance_table):
         
@@ -33,45 +35,35 @@ class Truck:
         return len(self.packages) == 0 and len(self.priority_packages) == 0
 
     def travel_to(self, location):
+        print(f"Truck {self.number} leaving {self.location} at {self.min_to_hr(self.current_time)}")
+
         distance = self.__distance_table.get_distance_between(self.location, location)
         self.update_time(distance)
         self.total_mileage += distance
         self.location = location
-        print(f"Traveled to {self.location}")
-        print(f"Current Time: {self.current_time}")
+
+        print(f"Truck {self.number} arrived at {self.location} at {self.min_to_hr(self.current_time)}")
 
     def deliver_package(self):
-        print(f"Truck: {self.number}")
-        print(f"Package: {self.current_delivery.id}")
-        print(f"Delivery Time: {self.current_time}")
+        print(f"Truck {self.number} delivered Package #{self.current_delivery.id} at {self.min_to_hr(self.current_time)}")
+        self.current_delivery.status = DeliveryStatus.DELIVERED
 
-    # deliver packages using the nearest neighbor algorithm
     def deliver_packages(self):
-        while (len(self.priority_packages) != 0):
-            lowest_distance = 10000
-            self.current_delivery = self.priority_packages
-            closest_package = min(
-                self.priority_packages,
-                key=lambda package: self.__distance_table.get_distance_between(
-                    self.location,
-                    package.full_address
-                )
-            )
+        self.nearest_neighbor_delivery(self.priority_packages)
+        self.nearest_neighbor_delivery(self.packages)
+        print()
 
-            self.current_delivery = closest_package
-            self.travel_to(self.current_delivery.full_address)
-            self.deliver_package()    
-            self.priority_packages.remove(self.current_delivery)
 
-        while (len(self.packages) != 0):
-            ...
+    def deliver_priority_packages(self):
+        self.nearest_neighbor_delivery(self.priority_packages)
+    
+    def deliver_non_priority_packages(self):
+        self.nearest_neighbor_delivery(self.packages)
 
     def nearest_neighbor_delivery(self, packages):
-        while (len(self.priority_packages) != 0):
-            lowest_distance = 10000
-            self.current_delivery = self.priority_packages
+        while (len(packages) != 0):
             closest_package = min(
-                self.priority_packages,
+                packages,
                 key=lambda package: self.__distance_table.get_distance_between(
                     self.location,
                     package.full_address
@@ -79,13 +71,26 @@ class Truck:
             )
 
             self.current_delivery = closest_package
+            self.current_delivery.status = DeliveryStatus.EN_ROUTE
             self.travel_to(self.current_delivery.full_address)
             self.deliver_package()    
-            self.priority_packages.remove(self.current_delivery)
-            return
+            packages.remove(self.current_delivery)
+            print()
+
 
     def update_time(self, distance):
         travel_time = (distance / self.speed) * 60 # in minutes
         self.current_time += travel_time 
     
-    
+    def min_to_hr(self, minutes):
+        hour = int(minutes // 60)
+        minute = int(minutes % 60)
+
+        period = "AM" if hour < 12 else "PM"
+
+        hour = hour % 12
+
+        if hour == 0:
+            hour = 12
+
+        return f"{hour}:{minute:02d} {period}"
--- main.py
+++ main.py
@@ -22,4 +22,38 @@ truck3 = Truck(3, 480, distance_table.addresses[0], distance_table)
 
 package_loader.load_trucks([truck1, truck2, truck3])
 
-truck1.deliver_packages()
+#truck 1 completes all deliveries
+truck1.deliver_priority_packages()
+truck1.deliver_non_priority_packages()
+truck1.travel_to(distance_table.addresses[0])
+print(f"TRUCK 1 HAS COMPLETED ALL DELIVERIES AT {truck1.min_to_hr(truck1.current_time)} WITH A TOTAL OF {truck1.total_mileage} MILES")
+print()
+print()
+
+
+#truck 2 delivers all packages with deadline and returns to hub
+truck2.deliver_priority_packages()
+truck2.travel_to(distance_table.addresses[0])
+print()
+print()
+
+#driver from truck 2 takes a 15 minute break and hops onto truck 3 to complete its packages, and returns to hub
+truck3.current_time = truck2.current_time
+truck3.current_time += 15
+truck3.deliver_priority_packages()
+truck3.deliver_non_priority_packages()
+print(f"TRUCK 3 HAS COMPLETED ALL DELIVERIES AT {truck3.min_to_hr(truck3.current_time)} WITH A TOTAL OF {truck3.total_mileage} MILES")
+print()
+print()
+
+
+#driver from truck 1 hops onto truck 2 to complete its non priority packages
+truck2.current_time = truck1.current_time
+truck2.deliver_non_priority_packages()
+truck2.travel_to(distance_table.addresses[0])
+print(f"TRUCK 2 HAS COMPLETED ALL DELIVERIES AT {truck2.min_to_hr(truck2.current_time)} WITH A TOTAL OF {truck2.total_mileage} MILES")
+print()
+print()
+
+total_miles = truck1.total_mileage + truck2.total_mileage + truck3.total_mileage
+print(f"ALL DELIVERIES COMPLETED IN {total_miles} MILES ")
